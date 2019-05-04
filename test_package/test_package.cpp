9a0312e80d476b42970195b915419127e4ae857b
Stephen Wray <wrayste+github.com@gmail.com>
2019-05-04 22:37:39 +0100

    Trim trailing whitespace for comparison

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,13 +1,26 @@
 #include <pprint.hpp>
 
+#include <algorithm>
+#include <cctype>
 #include <iostream>
+#include <locale>
+
+void rtrim(std::string &s)
+{
+    const auto isNotSpace = [](const int ch) {
+        return !std::isspace(ch);
+    };
+
+    s.erase(std::find_if(std::rbegin(s), std::rend(s), isNotSpace).base(), std::end(s));
+}
 
 int main()
 {
     std::stringstream stream;
     pprint::PrettyPrinter printer(stream);
     printer.print(3.14f);
-    const std::string result = stream.str();
+    std::string result = stream.str();
+    rtrim(result);
     std::cout << "Result = [" << result << "]" << std::endl;
     return result == "3.14f" ? 0 : 1;
 }
